9683a8795abe1ba16d9d401387223031a5a7e136
Lance0226 <lance@lances-Mac-mini.local>
2015-08-19 16:32:22 +0800

    fgfg
    dfdfd

--- src/main/java/com/lance/app/App.java
+++ src/main/java/com/lance/app/App.java
@@ -16,14 +16,16 @@ public class App
     
     
 
-	private static Map<String, String> info;
+	//private static Map<String, String> info;
 
 	public static void main( String[] args ) throws IOException
     {
 		Parse parse=new Parse("http://www.xuanran001.com/rwrule/zxfazzjfb3/409a55a7-86ae-4db4-99f8-fdbe94f30bef.html");
 		//info = new HashMap<String, String>();
 		//info=parse.GetInfo();
-		parse.GetBudget();
+		parse.GetBudget(0,"sgBill");
+		parse.GetBudget(1,"yzBill");
+		parse.GetBudget(2, "rzBill");
         	
         
     }
--- src/main/java/com/lance/parse/Parse.java
+++ src/main/java/com/lance/parse/Parse.java
@@ -2,6 +2,7 @@ package com.lance.parse;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,14 +13,16 @@ import org.jsoup.select.Elements;
 
 public class Parse 
 {
-  private static Map<String, String> projectInfo;
+  private        Map<String, String> projectInfo;
   private        Document            doc;
   private        List<Elements>      listBudgets;
 	
   public Parse(String strURL) throws IOException
   {
 	  this.projectInfo=new HashMap<String, String>();
-	  doc=Jsoup.connect(strURL).get();
+	  doc=Jsoup.connect(strURL).timeout(100000).get();  //设置10秒超时
+	  this.listBudgets=new LinkedList<Elements>();
+	  GetBudgets();
   }
   
   public Map<String,String> GetInfo()
@@ -73,7 +76,7 @@ public class Parse
 	  System.out.println("http://www.xuanran001.com"+panos.last().attr("data-swf")+"\n");
   }
   
-  private List<Elements> GetBudgets()
+  private void GetBudgets()
   {
 	  Elements budgets1=doc.select("section[id=spolo-sgBill]");
 	  Elements budgets2=doc.select("section[id=spolo-yzBill]");
@@ -83,19 +86,56 @@ public class Parse
 	  this.listBudgets.add(budgets2);
 	  this.listBudgets.add(budgets3);
 	  
-	  return this.listBudgets;
   }
 
-  public void GetBudget()
+  public void GetBudget(int index,String abr)
   {
-	  Elements budgets1=doc.select("section[id=spolo-sgBill]");
-	  System.out.println(budgets1.last().getElementsByClass("spolo-title").text());
+
+	  //家装基础施工预算
+	  System.out.println(this.listBudgets.get(index).last().getElementsByClass("spolo-title").text()+"\n");
 	  
-	  Elements subTitle1s=budgets1.last().select("a[data-parent=#accordion-sgBill]");
-	  for(Element subTitle1:subTitle1s)
+	  Elements subTitle0s=this.listBudgets.get(index).last().select("a[data-parent=#accordion-"+abr+"]");
+	  for(Element subTitle0:subTitle0s)
 	  {
-		  System.out.println(subTitle1.text()+"\n");
+		  String arr_str[]=subTitle0.text().split(" ");
+		  System.out.println(arr_str[0]+"\n");                                 //获得工程名称
+		  String project_name=arr_str[0].substring(0, arr_str[0].length()-2);  //去除工程两个字
+		  System.out.println(project_name);    
+		  System.out.println(arr_str[1]+"\n");
+		  
+		  Element temp=this.listBudgets.get(index).last().select("div[id="+abr+"-"+project_name+"]").last();
+		  Elements node2s=null;
+		  if(temp!=null)
+		  {
+		    node2s=temp.getElementsByTag("tr");//装修项目
+		  }
+		  
+		  if(node2s!=null)
+		  {
+		  for(Element node2:node2s)
+		  {
+			  Elements node3_1s=node2.select("th"); //项目的具体选项
+			  if(node3_1s!=null)
+			  {
+		         for (Element node3:node3_1s)
+		         {
+		    	   System.out.println(node3.text()+"\n");
+		         }
+			  }
+		      
+		      Elements node3_2s=node2.select("td");
+		      if(node3_2s!=null)
+		      {
+		    	  for(Element node3:node3_2s)
+		    	  {
+		    		System.out.println(node3.text()+"\n");  
+		    	  }
+		      }
+		    
+		  }
+		  }
 	  }
+	  
 	  
   }
 }
